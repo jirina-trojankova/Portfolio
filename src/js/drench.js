d1fd334e9e9b6dfec14e98a6151adb666b3f1ae9
zubr <jirina.trojankova@seznam.cz>
2018-04-14 16:37:41 +0200

    First two squares compare themselves

--- src/js/drench.js
+++ src/js/drench.js
@@ -1,42 +1,105 @@
-var div = '<div class="square"></div>';
+/*var div = '<div class="square"></div>';*/
 var rowOpen = '<div class="row">';
 var rowClose = '</div>';
 var chessboardSize = 10;
+var idNumber = 0;
+
+function getNumber() {
+var number = Math.floor((Math.random() * 3) + 1);
+return number;
+}
+
 
 for (j=1; j<=chessboardSize; j++) {
 	document.write(rowOpen);
   for (i=1; i<=chessboardSize; i++) {
-
-   document.write(div);
+   idNumber++;
+   document.write('<div class=\"square color' +getNumber()+ '\" id=\"' + idNumber +'\"></div>');
   };
         document.write(rowClose);
 };
 
+var id = 1;
+var square = document.getElementById(id);
+square = (square.classList);
+
+
+var right = document.getElementById(id + 1);
+right = (right.classList);
+
+
+if (JSON.stringify(square) === JSON.stringify(right)) {
+  console.log(JSON.stringify(square));
+  console.log(JSON.stringify(right));
+  square = document.getElementById(id);
+  square.className += " drench";
+
+  right = document.getElementById(id + 1);
+}
+console.log(square);
+console.log(right);
+
+
+
+
+
+/*var idString = id.toString();*/
 /*
+var bottom = document.getElementById('11');
+right.setAttribute('drench');
 document.getElementById("demo").innerHTML = text;
 */
 
-var arr = ["#fcfaef", "#fffa05", "#f7c3cd", "#f4d442", "#e6b0f2", "#e5c76e"]
-/*var arr = { "white" : "#fcfaef", "yellow" : "#fffa05", "pink": "#f7c3cd", "orange" : "#f4d442", "purple" : "#e6b0f2", "brown": "#e5c76e", }; 
+/*var arr = ["#fcfaef", "#fffa05", "#f7c3cd", "#f4d442", "#e6b0f2", "#e5c76e"]
+var arr = { "white" : "#fcfaef", "yellow" : "#fffa05", "pink": "#f7c3cd", "orange" : "#f4d442", "purple" : "#e6b0f2", "brown": "#e5c76e", }; 
 */
-
-
 
 function getColor() {
   var number = Math.floor((Math.random() * 6) + 1);
   var square = document.getElementById('square');
   square.style.background = ('color' + number);
-console.log(number);
-console.log(square);
-console.log(square.style.background);
 }
+
+
 
 
 function changeColor(newColor) {
-  var square = document.getElementById('square');
-  square.style.background = newColor;
-console.log(square);
-console.log(square.style.background);
+  var square = document.getElementById('1');
+
+  if(square.classList.contains('color1')) {
+    square.classList.remove('color1');
+  }else if(square.classList.contains('color2')) {
+    square.classList.remove('color2');
+  }else if(square.classList.contains('color3')) {
+    square.classList.remove('color3');
+  }else if(square.classList.contains('color4')) {
+    square.classList.remove('color4');
+  }else if(square.classList.contains('color5')) {
+    square.classList.remove('color5');
+  }else if(square.classList.contains('color6')) {
+    square.classList.remove('color6');
+  }
+  square.classList.add(newColor);
 }
+
+
+ 
+function makeDrench() {
+  var square = document.getElementById('1');
+ square = (square.classList);
+
+var drench = document.getElementsByClassName('drench')[0];
+
+  var right = document.getElementById('2');
+  var bottom = document.getElementById('11');
+  right = (right.classList);
+}
+
+
+
+/*if(square.classList.contains('drench') !=true) {
+  square.className += " drench";*/
+
+
 
 
